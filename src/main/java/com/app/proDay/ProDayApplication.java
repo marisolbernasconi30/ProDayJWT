dd165476afef6e9b44842f052c7f290dbacc1a5a
marisolbernasconi30 <usa2018sb1030@gmail.com>
2026-04-08 12:03:20 -0300

    first commit

---
+++ src/main/java/com/app/proDay/ProDayApplication.java
@@ -0,0 +1,13 @@
+package com.app.proDay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProDayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProDayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/proDay/ProDayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.proDay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProDayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
